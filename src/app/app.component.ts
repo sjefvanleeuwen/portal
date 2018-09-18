1ca74e4d66538f8b44f02e0ec2f9e5998a8fba13
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-09-18 23:18:14 +0200

    message service test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { MessageService } from './services/message.service';
+import { interval, Subject } from 'rxjs';
 
 @Component({
-  // tslint:disable-next-line
+  // tslint:disable-next-line:component-selector
   selector: 'body',
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: MessageService) {
+    this.a.subscribe(n => this.sendMessage());
+   }
+
+   a = interval(1000);
+
+  sendMessage(): void {
+    // send message to subscribers via observable subject
+    this.service.sendMessage('Message from app Component to message Component!');
+  }
+
+  clearMessage(): void  {
+    this.service.clearMessage();
+  }
 
   ngOnInit() {
     this.router.events.subscribe((evt) => {
