7bc3f76b7f6db0ffdd8cade151bbc9740ff22873
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-09-18 23:19:44 +0200

    propagate connected event after signalr connects in global

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,17 +1,21 @@
 import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+import { DefaultLayoutComponent } from './containers';
+import { inject } from '@angular/core/testing';
 
-class signalr {
+class SignalR {
     hubConnection: HubConnection;
-    constructor(){
+    lastTaskData = '';
+    constructor() {
         this.hubConnection = new HubConnectionBuilder()
             .withUrl('http://localhost:5051/eventhub')
             .build();
-        this.hubConnection.start().catch(err => document.write(err)).then(() => {
+        this.hubConnection.start().catch(err => document.write('Error connecting to signalr eventhub')).then(() => {
             this.hubConnection.invoke('Subscribe', 'topic');
-     });
+            this.hubConnection.invoke('Connected');
+        });
     }
-};
-  
+}
+
 export const global = {
-    signalr: new signalr()
+    signalr: new SignalR()
 };
