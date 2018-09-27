5a8d7a01d5e9dd0e87ea8dff2a069b64d5588c63
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-09-27 11:54:59 +0200

    irma verificatie (wip) en issue (werkt)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { interval, Subject } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -12,6 +13,8 @@ export class AppComponent implements OnInit {
    }
 
   ngOnInit() {
+    let headers = new HttpHeaders();
+    headers = headers.append('Access-Control-Allow-Origin', '*');
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
         return;
--- src/app/views/irma/irma.component.ts
+++ src/app/views/irma/irma.component.ts
@@ -1,32 +1,126 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import * as jwtDecode from 'jwt-decode';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
-
-
-interface TokenDto {
-  foo: string;
-  exp: number;
-  iat: number;
-}
-
-interface TokenHeaderDto {
-  typ: string;
-  alg: string;
-}
 @Component({
   selector: 'app-irma',
   templateUrl: './irma.component.html',
   styleUrls: ['./irma.component.scss']
 })
+
 export class IrmaComponent implements OnInit {
 
-  // tslint:disable-next-line:max-line-length
-  JWTTest = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJkaXNjbG9zdXJlX3Jlc3VsdCIsImlzcyI6ImlybWFfYXBpX3NlcnZlciIsImF0dHJpYnV0ZXMiOnsicGJkZi5wYmRmLmlkaW4uZmFtaWx5bmFtZSI6InZhbiBMZWV1d2VuIn0sImV4cCI6MTUzNzc5MTMxNSwiaWF0IjoxNTM3NzkxMjU1LCJqdGkiOm51bGwsInN0YXR1cyI6IlZBTElEIn0.TkCDRH9Xc1vXAZtDr4DmjOJXbKfyk2nv9gPjKZCZnTFBwEm1sadnULzHAibsITb2n5YCVQ6bYXNSxLTIbJcYhorMqsPy4ft1crS2DsACHC6TIbS7SqpW8dSCx12bzD-LMQZeLNQBZfqI4MNvfcqy3GTII80q7k_hO-aauz5ODcRH4Ivlw3MzVfLpxrZWJ5pdJzlMvy8nhdZAObJ0uvYKKicjitqQokt_x_X1W_BhFpDtpoP1kTxhzhxjJ2iHClmCUH_w2w0Ofi2mEFjFRlyr6KAIxOArrNI49rQEKFiwI30f013_oJ_wbjWWc_Frt49Bzbl1iRhAMfT7vB7u2chukw';
+  url = 'https://privacybydesign.foundation/tomcat/irma_api_server/api/v2/issue/';
+  JWTTest = '';
+  header: any;
+  payload: any;
+  signature: any;
 
-  constructor() { }
+  p1: string;
+  p2: string;
 
-  ngOnInit() {
-   console.log(jwtDecode(this.JWTTest).attributes['pbdf.pbdf.idin.familyname']);
+  irma_api_server_address = 'http://10.109.0.178:8088';
+
+  iat = Math.round((new Date()).getTime() / 1000) + 120;
+
+  headers = new HttpHeaders({
+    'Content-Type': 'text/plain',
+    'Accept' : 'application/json, text/plain'
+  });
+
+  constructor(private renderer: Renderer2, private httpClient: HttpClient)  { }
+
+  disclose() {
+    let header = {
+      "alg": "none",
+      "typ" : "JWT"
+    };
+
+    let payload = {
+        "iat":  this.iat,
+        "sub": "verification_request",
+        "iss": "wigo4it",
+        "sprequest": {
+          "validity": 1569456000,
+          "request": {
+            "content": [
+              {
+                "label": "burgerservicenummer",
+                "attributes": [
+                  "irma-demo.nijmegen.bsn.bsn"
+                ]
+              }
+            ]
+          }
+        }
+      };
+
+    const p1 = btoa(JSON.stringify(header)).replace(/=/g, '');
+    const p2 = btoa(JSON.stringify(payload)).replace(/=/g, '');
+
+    this.httpClient.post('http://0.0.0.0:8088/api/v2/verification',
+      p1 + '.' + p2 + '.', {headers: this.headers}).subscribe((result: Object) => {
+        //  const clone = Object.create(result);
+          result.u = this.irma_api_server_address  + '/api/v2/verification/' + result.u;
+          this.JWTTest = JSON.stringify(result);
+          // this.httpClient.get('http://10.109.0.178:8088/api/v2/verification/' + clone.u + '/status?' + Math.random())
+          //   .subscribe((innerResult: Object) => {
+          //   alert(innerResult) ;
+          // });
+    });
   }
 
+  public clone(): any {
+    const cloneObj = new (<any>this.constructor());
+    for (const attribut in this) {
+        if (typeof this[attribut] === 'object') {
+            cloneObj[attribut] = this.clone();
+        } else {
+            cloneObj[attribut] = this[attribut];
+        }
+    }
+    return cloneObj;
+}
+
+  issue() {
+    let header = {
+      "alg": "none",
+      "typ" : "JWT"
+    }
+ 
+    let payload = {
+      "iss": "wigo4it",
+      "iat": this.iat,
+      "sub": "issue_request",
+      "iprequest": {
+        "timeout": 120,
+        "data": "",
+        "request": {
+          "credentials": [
+            {
+              "validity": 1569456000,
+              "keyCounter": 0,
+              "credential": "irma-demo.wigo4it.stadspas",
+              "attributes": {
+                "stadspas": "yes"
+              }
+            }
+          ],
+          "disclose": []
+        }
+      }
+    };
+    this.p1 = btoa(JSON.stringify(header));
+    this.p2 = btoa(JSON.stringify(payload));
+
+    this.httpClient.post('http://0.0.0.0:8088/api/v2/issue',
+      this.p1 + '.' + this.p2 + '.', {headers: this.headers}).subscribe((result: Object) => {
+          result.u = this.irma_api_server_address  + '/api/v2/issue/' + result.u;
+          this.JWTTest = JSON.stringify(result);
+    });
+
+
+
+  ngOnInit() {
+  }
 }
